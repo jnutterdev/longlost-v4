c9491091a79fbda9dcc1119b78ecf82ea9c0cd5d
jnutterdev <hello@jnutterdev.com>
2026-06-17 13:04:58 -0400

    fixing up hosted site

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '76a6ea953bc357c340b6b27801fc4d2cf97e3113', queries,  });
+export const client = createClient({ cacheDir: '/Users/chromia/github.com/jnutterdev/longlost-v4/tina/__generated__/.cache/1781715867487', url: 'https://content.tinajs.io/2.4/content/afb6807c-8c9e-4d95-b37f-1d7b54c4df66/github/main', token: '76a6ea953bc357c340b6b27801fc4d2cf97e3113', queries,  });
 export default client;
   
--- tina/__generated__/types.js
+++ tina/__generated__/types.js
@@ -257,7 +257,7 @@ const generateRequester = (client) => {
 export const ExperimentalGetTinaClient = () => getSdk(
   generateRequester(
     createClient({
-      url: "http://localhost:4001/graphql",
+      url: "https://content.tinajs.io/2.4/content/afb6807c-8c9e-4d95-b37f-1d7b54c4df66/github/main",
       queries
     })
   )
