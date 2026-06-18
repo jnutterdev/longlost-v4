5a0276589fc75e8545be87b1d5faf642615862c1
jnutterdev <hello@jnutterdev.com>
2026-06-18 07:37:10 -0400

    Switching to astro-tina plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import tina from 'astro-tina';
 
 export default defineConfig({
   site: 'https://longlostforgotten.com',
   output: 'static',
-  integrations: [react()],
+  integrations: [react(), tina()],
 });
--- tina/__generated__/types.js
+++ tina/__generated__/types.js
@@ -260,7 +260,7 @@ const generateRequester = (client) => {
 export const ExperimentalGetTinaClient = () => getSdk(
   generateRequester(
     createClient({
-      url: "https://content.tinajs.io/2.4/content/afb6807c-8c9e-4d95-b37f-1d7b54c4df66/github/main",
+      url: "http://localhost:4001/graphql",
       queries
     })
   )
