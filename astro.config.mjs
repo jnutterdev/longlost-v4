cd44253db898daa978a48e7cf036ed5c930d56b0
jnutterdev <hello@jnutterdev.com>
2026-06-17 22:50:30 -0400

    Setting site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://longlostforgotten.com',
   output: 'static',
   integrations: [react()],
 });
